11b181ad70e3616d31d508c22a5f83027a38736b
amin <amin@LAPTOP-GQQANM6K>
2021-04-25 22:36:36 +0200

    solve conflicts between android stidio ide and intellija ide solve conflicts between ARcore and google scence plugin first working ar object

--- app/src/main/java/com/example/aic/activitiesCode/came2.java
+++ app/src/main/java/com/example/aic/activitiesCode/came2.java
@@ -1,28 +1,128 @@
 package com.example.aic.activitiesCode;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
-
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 import com.example.aic.R;
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
 
 public class came2 extends AppCompatActivity {
+    private static final String TAG = came2.class.getSimpleName();
+    private static final double MIN_OPENGL_VERSION = 3.0;
+
     private ModelRenderable andyRenderable;
     private ArFragment arFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (!checkIsSupportedDeviceOrFinish(this)) {
+            return;
+        }
         setContentView(R.layout.activity_came2);
 
         arFragment=(ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
-        setUpModel();
+//        setUpModel();
+//        setUpPlane();
+
+        ModelRenderable.builder()
+                .setSource(this, R.raw.wolves)
+                .build()
+                .thenAccept(renderable -> andyRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast toast =
+                                    Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                            return null;
+                        });
+
+
+        arFragment.setOnTapArPlaneListener((
+                HitResult hitResult,Plane plane,MotionEvent motionEvent) -> {
+
+            if (andyRenderable == null) {
+                return;
+            }
+            Anchor anchor = hitResult.createAnchor();
+            AnchorNode anchorNode = new AnchorNode(anchor);
+            anchorNode.setParent(arFragment.getArSceneView().getScene());
+            TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+            node.setParent(anchorNode);
+            node.setRenderable(andyRenderable);
+            node.select();
+
+        });
     }
 
+    private void setUpPlane() {
+        arFragment.setOnTapArPlaneListener((
+               HitResult hitResult,Plane plane,MotionEvent motionEvent) -> {
+            if (andyRenderable == null) {
+                return;
+            }
+            Anchor anchor = hitResult.createAnchor();
+            AnchorNode anchorNode = new AnchorNode(anchor);
+            anchorNode.setParent(arFragment.getArSceneView().getScene());
+            TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+            node.setParent(anchorNode);
+            node.setRenderable(andyRenderable);
+            node.select();
+
+        });
+    }
+
+
+
     private void setUpModel() {
-//        ModelRenderable.builder()
-//                .setSource(this,R.raw.)
+        ModelRenderable.builder()
+                .setSource(this, R.raw.wolves)
+                .build()
+                .thenAccept(renderable -> andyRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast toast =
+                                    Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                            return null;
+                        });
+
+    }
+
+    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            Log.e(TAG, "Sceneform requires Android N or later");
+            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+            activity.finish();
+            return false;
+        }
+        String openGlVersionString =
+                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show();
+            activity.finish();
+            return false;
+        }
+        return true;
     }
 }

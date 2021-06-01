2f97ec339d9daa77dc6f407f2b3370c8fe7c871c
amin <amin@LAPTOP-GQQANM6K>
2021-06-02 01:07:05 +0200

    adding progress bar

--- app/src/main/java/com/example/aic/activitiesCode/projects.java
+++ app/src/main/java/com/example/aic/activitiesCode/projects.java
@@ -2,16 +2,20 @@ package com.example.aic.activitiesCode;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.ProgressBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.aic.R;
 
 public class projects extends AppCompatActivity {
-
+ProgressBar bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_projects);
+
+        bar =findViewById(R.id.progressBar1);
+        bar.setVisibility(View.INVISIBLE);
     }
 
     public void AR(View view) {
@@ -21,12 +25,19 @@ public class projects extends AppCompatActivity {
     }
 
     public void _3d(View view) {
+       bar.setVisibility(View.VISIBLE);
         Intent in = new Intent(this, _3D_objectViewer.class);
         startActivity(in);
 
     }
 
-    public void view3d(View view) {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        bar.setVisibility(View.INVISIBLE);
 
     }
+
+
+
 }
